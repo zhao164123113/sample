deab7499de93c90b2430c890167fc25aac97e61d
Miner <164123113@qq.com>
2018-05-09 15:19:30 +0000

    Updated database configuration

--- config/database.php
+++ config/database.php
@@ -60,7 +60,7 @@ return [
             'driver' => 'pgsql',
             'host' => $db_config['host'],
             'port' => env('DB_PORT', '5432'),
-            'database' => $db_config['host'],
+            'database' => $db_config['database'],
             'username' => $db_config['username'],
             'password' => $db_config['password'],
             'charset' => 'utf8',
