792bfeeae328757ba9eb0daf64b753c4c33dc184
Miner <164123113@qq.com>
2018-05-11 07:11:38 +0000

    Add user statuses

--- resources/views/shared/_user_info.blade.php
+++ resources/views/shared/_user_info.blade.php
@@ -1,4 +1,4 @@
 <a href="{{ route('users.show', $user->id) }}" style="display: inherit; text-align: center;">
-  <img src="{{ $user->gravatar('140') }}" alt="{{ $user->name }}" class="gravatar"/>
+  <img src="{{ $user->gravatar('100') }}" alt="{{ $user->name }}" class="gravatar" style="border-radius: 50px" />
 </a>
 <h1>{{ $user->name }}</h1>
